1c9600ab12c8aff18034e0af71c21942c1c112b3
Selçuk Özdemir <selcuk-ozdemir79@hotmail.com>
2024-04-16 01:01:11 +0300

    added example templates for cli

---
+++ createDirectoryContents.js
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+const CURR_DIR = process.cwd();
+
+const createDirectoryContents = (templatePath, newProjectPath) => {
+  const filesToCreate = fs.readdirSync(templatePath);
+
+  filesToCreate.forEach(file => {
+    const origFilePath = `${templatePath}/${file}`;
+
+    const stats = fs.statSync(origFilePath);
+
+    if (stats.isFile()) {
+      const contents = fs.readFileSync(origFilePath, 'utf8');
+
+      if (file === '.npmignore') file = '.gitignore';
+
+      const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
+      fs.writeFileSync(writePath, contents, 'utf8');
+    } else if (stats.isDirectory()) {
+      fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
+
+      createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+    }
+  });
+};
+
+export default createDirectoryContents;
