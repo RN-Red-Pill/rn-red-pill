1ec4315741f8d68656ea1d323d68338e51e9fcc8
vfurkanguner <vfurkanguner@gmail.com>
2024-04-27 23:57:49 +0300

    casdasd

--- templates/red-pill-typescript/commitlint.config.js
+++ templates/red-pill-typescript/commitlint.config.js
@@ -1,1 +1,23 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [2, 'always', 140],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'ui',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'chore',
+                'revert',
+                'ci'
+            ]
+        ]
+    }
+};
